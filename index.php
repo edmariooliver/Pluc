62e9f54d1513b76b0e9ad4346dc3e1b489a602ad
edmariooliver <barrigacraft@hotmail.com>
2020-12-11 16:48:59 -0300

    Teste no deploy

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 
     //Error => false
 
-    ini_set('display_errors', 0 );
-    error_reporting(0);
+    // ini_set('display_errors', 0 );
+    // error_reporting(0);
     
     //loading archive whu we be will using the class "Link"
 
