3d0a740035e8cb197ecb4a10fff789dcf7d8363c
edmariooliver <49793161+edmariooliver@users.noreply.github.com>
2020-12-11 17:16:59 -0300

    Add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7d919ebf1469829b5dfb0d2c9b2a95baf1880ae7',
+    'name' => 'vendor/encurtador_link',
+  ),
+  'versions' => 
+  array (
+    'chillerlan/php-qrcode' => 
+    array (
+      'pretty_version' => '4.3.0',
+      'version' => '4.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4968063fb3baeedb658293f89f9673fbf2499a3e',
+    ),
+    'chillerlan/php-settings-container' => 
+    array (
+      'pretty_version' => '2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '90ab24a3dc09569bfa0f3dbc3d44bab3aaa2a6c5',
+    ),
+    'vendor/encurtador_link' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7d919ebf1469829b5dfb0d2c9b2a95baf1880ae7',
+    ),
+  ),
+);
